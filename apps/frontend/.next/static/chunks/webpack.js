218a33522c716fbff48aedaaa75cecf0cd93147c
ordamari <ordamari971@gmail.com>
2023-04-14 13:00:50 +0300

    Add next to git ignore

--- apps/frontend/.next/static/chunks/webpack.js
+++ apps/frontend/.next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "75d5e85a28a69804"; }
+/******/ 		__webpack_require__.h = function() { return "9ca7b13c7b78c918"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
