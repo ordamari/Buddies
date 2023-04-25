629602dae8c86c452979b480c678e0869b54a944
Niv Stibel <Nivsvb@gmail.com>
2023-04-25 10:36:31 +0300

    fix the issue the fileuploader not found

--- apps/backend/src/users/resolvers/users/users.resolver.ts
+++ apps/backend/src/users/resolvers/users/users.resolver.ts
@@ -2,9 +2,8 @@ import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { ActiveUser } from 'src/iam/decorators/active-user.decorator';
 import { ActiveUserData } from 'src/iam/interfaces/active-user-data.interface';
 import { User } from 'src/users/entities/user.entity';
-// import GraphQLUpload from 'graphql-upload/GraphQLUpload.mjs';
-// import { FileUpload } from 'graphql-upload/processRequest.mjs';
-import { GraphQLUpload, FileUpload } from 'graphql-upload';
+import GraphQLUpload from 'graphql-upload/GraphQLUpload.mjs';
+import { FileUpload } from 'graphql-upload/Upload.mjs';
 
 @Resolver()
 export class UsersResolver {
@@ -14,7 +13,7 @@ export class UsersResolver {
     @Args('file', { type: () => GraphQLUpload })
     { createReadStream, filename }: FileUpload,
   ) {
-    createReadStream().pipe();
+    // createReadStream().pipe();
     // return this.postsService.update(id, updatePostInput);
   }
 }
