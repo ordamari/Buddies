2dcc1fcfd6b006634c5239a82f5ac7bdb28a5fbc
ordamari <ordamari971@gmail.com>
2023-04-25 10:27:01 +0300

    small changes

--- apps/backend/src/users/resolvers/users/users.resolver.ts
+++ apps/backend/src/users/resolvers/users/users.resolver.ts
@@ -1,4 +1,20 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { ActiveUser } from 'src/iam/decorators/active-user.decorator';
+import { ActiveUserData } from 'src/iam/interfaces/active-user-data.interface';
+import { User } from 'src/users/entities/user.entity';
+// import GraphQLUpload from 'graphql-upload/GraphQLUpload.mjs';
+// import { FileUpload } from 'graphql-upload/processRequest.mjs';
+import { GraphQLUpload, FileUpload } from 'graphql-upload';
 
 @Resolver()
-export class UsersResolver {}
+export class UsersResolver {
+  @Mutation(() => User, { name: 'updateProfileImage' })
+  async update(
+    @ActiveUser() activeUser: ActiveUserData,
+    @Args('file', { type: () => GraphQLUpload })
+    { createReadStream, filename }: FileUpload,
+  ) {
+    createReadStream().pipe();
+    // return this.postsService.update(id, updatePostInput);
+  }
+}
