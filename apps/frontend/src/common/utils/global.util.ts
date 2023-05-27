34ac6c97f7b5a0001cbbcd187758c8fddfa99631
ordamari <ordamari971@gmail.com>
2023-05-27 20:16:43 +0300

    Add reed more less components, finish style for mobile

--- apps/frontend/src/common/utils/global.util.ts
+++ apps/frontend/src/common/utils/global.util.ts
@@ -1,6 +1,8 @@
 export const globalUtil = {
   setCssVariables,
   getUniqueStringArray,
+  findSpaceIndexesBetweenPoints,
+  callFnsInSequence,
 };
 
 function setCssVariables(data: Record<string, string>) {
@@ -13,4 +15,29 @@ function getUniqueStringArray(stringArray: string[]) {
   return stringArray.filter(function (string, idx) {
     return stringArray.indexOf(string) == idx;
   });
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+function callFnsInSequence(...fns: Function[]) {
+  return (...args: any[]) => {
+    fns.forEach((fn) => fn && fn(...args));
+  };
+}
+
+function findSpaceIndexesBetweenPoints(
+  input: string,
+  startIndex?: number,
+  endIndex?: number,
+) {
+  startIndex = startIndex || 0;
+  endIndex = endIndex || input.length;
+  const spaceIndexes: number[] = [];
+  let currentIndex = input.indexOf(' ', startIndex);
+
+  while (currentIndex !== -1 && currentIndex < endIndex) {
+    spaceIndexes.push(currentIndex);
+    currentIndex = input.indexOf(' ', currentIndex + 1);
+  }
+
+  return spaceIndexes;
 }
